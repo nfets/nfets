e41eac9b30d735acc27157e96a84df2305c0e4db
Gustavo Lidani <gustavo@lidani.dev>
2025-02-06 22:41:18 -0300

    shared: adds either monad

---
+++ packages/core/src/shared/either.ts
@@ -0,0 +1,46 @@
+export type Either<L, R> = Left<L> | Right<R>;
+
+export class Left<L> {
+  readonly value: L;
+
+  constructor(value: L) {
+    this.value = value;
+  }
+
+  isLeft(): this is Left<L> {
+    return true;
+  }
+
+  isRight(): this is never {
+    return false;
+  }
+}
+
+export class Right<R> {
+  readonly value: R;
+
+  constructor(value: R) {
+    this.value = value;
+  }
+
+  isLeft(): this is never {
+    return false;
+  }
+
+  isRight(): this is Right<R> {
+    return true;
+  }
+}
+
+export const left = <L = never>(l: L): Left<L> => new Left<L>(l);
+
+export const right = <R = never>(a?: R): Right<R> =>
+  a === void 0 ? new Right<R>(void 0 as R) : new Right<R>(a);
+
+export const isRight = <L, R>(either: Either<L, R>): either is Right<R> => {
+  return either instanceof Right;
+};
+
+export const isLeft = <L, R>(either: Either<L, R>): either is Left<L> => {
+  return either instanceof Left;
+};
