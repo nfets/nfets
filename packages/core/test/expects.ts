6fce35861b2d22719aa48c9afca11c429f9f974e
Gustavo Lidani <gustavo@lidani.dev>
2025-02-12 14:24:15 -0300

    test: either assertions for jest

---
+++ packages/core/test/expects.ts
@@ -0,0 +1,19 @@
+import {
+  isLeft,
+  isRight,
+  type Left,
+  type Right,
+  type Either,
+} from 'src/shared/either';
+
+export const expectIsRight: <L, A>(
+  result: Either<L, A>,
+) => asserts result is Right<A> = (result) => {
+  expect(isRight(result)).toBeTruthy();
+};
+
+export const expectIsLeft: <L, A>(
+  result: Either<L, A>,
+) => asserts result is Left<L> = (result) => {
+  expect(isLeft(result)).toBeTruthy();
+};
