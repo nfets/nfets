ce16c7ab18eb1401fbe4ee040c2dc1a740f80529
Gustavo Lidani <gustavo@lidani.dev>
2025-02-20 08:58:37 -0300

    prevent integration tests without a valid certificate

--- packages/core/test/infrastructure/repositories/soap-remote-transmission-repository.spec.ts
+++ packages/core/test/infrastructure/repositories/soap-remote-transmission-repository.spec.ts
@@ -5,26 +5,29 @@ import path from 'node:path';
 import { MemoryCacheAdapter } from 'src/infrastructure/repositories/memory-cache-adapter';
 import { NodeCertificateRepository } from 'src/infrastructure/repositories/node-certificate-repository';
 import { SoapRemoteTransmissionRepository } from 'src/infrastructure/repositories/soap-remote-transmission-repository';
+import { ensureIntegrationTestsHasValidCertificate } from '@nfets/test/ensure-integration-tests';
 
 const certificatePath = process.env.TEST_CERTIFICATE_PATH,
   password = process.env.TEST_CERTIFICATE_PASSWORD;
 
-if (certificatePath && password !== undefined)
-  describe('soap remote transmission nfe (integration) (not destructive)', () => {
-    const transmission = new SoapRemoteTransmissionRepository(
-      path.resolve(__dirname, '../../../', 'nfe', 'schemas'),
-      new NodeCertificateRepository(axios.create(), new MemoryCacheAdapter()),
-    );
+describe('soap remote transmission nfe (integration) (not destructive)', () => {
+  if (!certificatePath || password === undefined)
+    return ensureIntegrationTestsHasValidCertificate();
 
-    it('should return schema failed when payload is invalid', async () => {
-      await transmission.setCertificate(certificatePath, password);
-      const response = await transmission.send();
+  const transmission = new SoapRemoteTransmissionRepository(
+    path.resolve(__dirname, '../../../', 'nfe', 'schemas'),
+    new NodeCertificateRepository(axios.create(), new MemoryCacheAdapter()),
+  );
 
-      expectIsRight(response);
-      expect(response.value).toMatchObject({
-        retConsStatServ: {
-          xMotivo: 'Rejeicao: Falha no schema XML',
-        },
-      });
+  it('should return schema failed when payload is invalid', async () => {
+    await transmission.setCertificate(certificatePath, password);
+    const response = await transmission.send();
+
+    expectIsRight(response);
+    expect(response.value).toMatchObject({
+      retConsStatServ: {
+        xMotivo: 'Rejeicao: Falha no schema XML',
+      },
     });
   });
+});
---
+++ packages/test/src/ensure-integration-tests.ts
@@ -0,0 +1,4 @@
+export const ensureIntegrationTestsHasValidCertificate = () => {
+  it.skip('for integration tests you must have a valid certificate recognized by SEFAZ', () =>
+    void 0);
+};
