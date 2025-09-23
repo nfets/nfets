fc186b05be6efc88596672adc41cf09d48bed80f
Gustavo Lidani <gustavo@lidani.dev>
2025-09-23 16:54:37 -0300

    não ha ferrolhos

--- packages/nfe/src/application/xml-builder/nfe-xml-builder.ts
+++ packages/nfe/src/application/xml-builder/nfe-xml-builder.ts
@@ -2,7 +2,7 @@ import type { InfNfe } from 'src/entities/nfe/inf-nfe';
 import type { NFeBuilder } from 'src/entities/xml-builder/nfe';
 
 export class NfeXmlBuilder implements NFeBuilder {
-  public infNFe(payload: InfNfe) {
+  public infNFe(_payload: InfNfe) {
     return void 0;
   }
 }
