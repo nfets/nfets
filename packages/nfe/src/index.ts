e3d3bb3eef0d5bb207ab89922a47f799e485aef2
Gustavo Lidani <gustavo@lidani.dev>
2025-09-25 15:15:42 -0300

    chore: update CI workflow and refactor NFe XML builder
    - Added environment variable `NFETS_ADDONS_DIR` to the CI workflow for improved path management.
    - Refactored the NFe XML builder to use a static `create` method for instantiation and updated type imports for better clarity and type safety.

--- packages/nfe/src/index.ts
+++ packages/nfe/src/index.ts
@@ -1,4 +1,4 @@
-import type { InfNFe } from './entities/nfe/inf-nfe';
+import type { InfNFeAttributes } from './entities/nfe/inf-nfe';
 import type { Ide } from './entities/nfe/inf-nfe/ide';
 import type { Emit } from './entities/nfe/inf-nfe/emit';
 import type { Det } from './entities/nfe/inf-nfe/det';
@@ -8,9 +8,9 @@ import { NfeXmlBuilder } from './application/xml-builder/nfe-xml-builder';
 import { Xml2JsBuilder } from '@nfets/core';
 
 export default (() => {
-  const builder = new NfeXmlBuilder(new Xml2JsBuilder());
+  const builder = NfeXmlBuilder.create(new Xml2JsBuilder());
   const xml = builder
-    .infNFe({} as InfNFe)
+    .infNFe({} as InfNFeAttributes)
     .ide({} as Ide)
     .emit({} as Emit)
     .det([] as Det[])
