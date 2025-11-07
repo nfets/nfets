60a1fdca2f116b2c8b1919fad37baec675daeae7
Gustavo Lidani <gustavo@lidani.dev>
2025-11-07 15:15:22 -0300

    feat: implement date toolkit and enhance date transformation
    - Added a new date toolkit using dayjs with UTC and timezone support.
    - Updated the TransformDateString function to utilize the new date toolkit for formatting date strings.
    - Integrated IsDateString validation to ensure proper date string formatting.

--- packages/core/src/application/validations/transformers/transform-date-string.ts
+++ packages/core/src/application/validations/transformers/transform-date-string.ts
@@ -1,6 +1,6 @@
 import { Transform } from 'class-transformer';
-import { type ValidationOptions } from 'class-validator';
-import dayjs from 'dayjs';
+import { IsDateString, type ValidationOptions } from 'class-validator';
+import date from '../../date/date-toolkit';
 
 export interface TransformToDateStringArgs extends ValidationOptions {
   format?: string;
@@ -10,7 +10,8 @@ export const TransformDateString = (args: TransformToDateStringArgs = {}) => {
   return (target: object, propertyKey: string | symbol) => {
     Transform(({ value }: { value?: Date | string }) => {
       const parsed = value instanceof Date ? value.toISOString() : value;
-      return parsed ? dayjs(parsed).format(args.format) : void 0;
+      return parsed ? date(parsed).utc().format(args.format) : void 0;
     })(target, propertyKey);
+    IsDateString()(target, propertyKey);
   };
 };
