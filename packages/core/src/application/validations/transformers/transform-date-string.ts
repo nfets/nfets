c38e4d0fbeda961555b6941af572f19df3eef4d6
Gustavo Lidani <gustavo@lidani.dev>
2025-11-17 11:32:45 -0300

    feat: add timezone mapping for Brazilian states and enhance date transformation logic
    - Introduced a new timezone mapping for Brazilian states to improve date handling based on regional time zones.
    - Updated the TransformDateString function to utilize the new timezone mapping, ensuring accurate date formatting for different states.
    - Added a TODO comment to address future improvements regarding context retrieval for timezone adjustments.

--- packages/core/src/application/validations/transformers/transform-date-string.ts
+++ packages/core/src/application/validations/transformers/transform-date-string.ts
@@ -10,7 +10,10 @@ export const TransformDateString = (args: TransformToDateStringArgs = {}) => {
   return (target: object, propertyKey: string | symbol) => {
     Transform(({ value }: { value?: Date | string }) => {
       const parsed = value instanceof Date ? value.toISOString() : value;
-      return parsed ? date(parsed).utc().format(args.format) : void 0;
+      // TODO: find a way to get the context from the validator to fill timezone for each state code
+      return parsed
+        ? date(parsed).tz('America/Sao_Paulo').format(args.format)
+        : void 0;
     })(target, propertyKey);
     IsDateString()(target, propertyKey);
   };
