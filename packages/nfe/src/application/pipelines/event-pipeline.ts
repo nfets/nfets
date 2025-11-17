148878063391133487e98ad161c2cbbf7ac95dba
Gustavo Lidani <gustavo@lidani.dev>
2025-11-17 11:50:02 -0300

    feat: implement EventPipeline and abstract Pipeline class for event processing
    - Introduced EventPipeline class to handle event processing with structured payloads and validation.
    - Created abstract Pipeline class to encapsulate common functionality for NFe processing, including HTTP client setup and certificate management.
    - Enhanced error handling and validation mechanisms within the event processing flow.

---
+++ packages/nfe/src/application/pipelines/event-pipeline.ts
@@ -0,0 +1,90 @@
+import type { TpEvent } from '@nfets/nfe/domain/entities/constants/tp-event';
+import type { EventoItem as IEventoItem } from '@nfets/nfe/domain/entities/services/evento';
+import type { NfeTransmitterOptions } from '@nfets/nfe/domain/entities/transmission/nfe-remote-client';
+
+import {
+  NFeTsError,
+  right,
+  type StateCode,
+  type Either,
+  Validates,
+  left,
+} from '@nfets/core';
+import events from '@nfets/nfe/services/events';
+import { Pipeline } from './pipeline';
+import { EventoItem } from '@nfets/nfe/infrastructure/dto/services/evento';
+
+export abstract class EventPipeline extends Pipeline {
+  protected readonly xmlns = 'http://www.portalfiscal.inf.br/nfe';
+
+  protected async event<T>(
+    tpEvento: TpEvent,
+    payload: Pick<
+      IEventoItem<T>['infEvento'],
+      'nSeqEvento' | 'dhEvento' | 'chNFe' | 'detEvento'
+    >,
+    options: Pick<NfeTransmitterOptions, 'tpAmb'>,
+  ): Promise<Either<NFeTsError, IEventoItem<{ descEvento: string } & T>>> {
+    const certificateOrLeft = await this.certificates.read(this.certificate);
+    if (certificateOrLeft.isLeft()) return certificateOrLeft;
+
+    const cUF = payload.chNFe.slice(0, 2) as StateCode;
+
+    this.transmitter.configure({
+      cUF,
+      ...options,
+      certificate: certificateOrLeft.value,
+    });
+
+    const metadata = events[tpEvento];
+
+    const info = this.certificates.getCertificateInfo(
+      certificateOrLeft.value.certificate,
+    );
+
+    const { nSeqEvento, dhEvento, chNFe } = payload;
+    const Id = [
+      'ID',
+      tpEvento,
+      chNFe,
+      nSeqEvento.toString().padStart(2, '0'),
+    ].join('');
+
+    const event = {
+      $: { xmlns: this.xmlns },
+      infEvento: {
+        $: { Id },
+        cOrgao: cUF,
+        tpAmb: options.tpAmb,
+        CNPJ: info.CNPJ,
+        CPF: info.CPF,
+        chNFe,
+        dhEvento,
+        tpEvento,
+        nSeqEvento,
+        verEvento: metadata.version,
+        detEvento: {
+          $: { versao: metadata.version },
+          descEvento: metadata.descEvento,
+          ...payload.detEvento,
+        },
+      },
+    };
+
+    const validatedOrLeft = this.validated(event);
+    if (validatedOrLeft.isLeft()) return validatedOrLeft;
+
+    return await this.signer.sign(
+      validatedOrLeft.value,
+      { tag: 'infEvento', mark: 'Id' },
+      certificateOrLeft.value,
+    );
+  }
+
+  @Validates<IEventoItem<{ descEvento: string }>>(EventoItem)
+  private validated<T>(payload: IEventoItem<{ descEvento: string } & T>) {
+    const errors = this.errors();
+    if (errors) return left(new NFeTsError(errors.join(', ')));
+    return right(payload);
+  }
+}
---
+++ packages/nfe/src/application/pipelines/pipeline.ts
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+import type {
+  CacheAdapter,
+  CertificateRepository,
+  HttpClient,
+  ReadCertificateRequest,
+  RemoteTransmissionRepository,
+  XmlToolkit,
+} from '@nfets/core/domain';
+import {
+  Xml2JsToolkit,
+  NativeCertificateRepository,
+  SoapRemoteTransmissionRepository,
+  MemoryCacheAdapter,
+} from '@nfets/core/infrastructure';
+import { EntitySigner, ValidateErrorsMetadata } from '@nfets/core/application';
+
+import type { NfeRemoteClient } from '@nfets/nfe/domain/entities/transmission/nfe-remote-client';
+import { NfeRemoteTransmitter } from '../transmission/nfe-transmitter';
+
+export abstract class Pipeline {
+  protected readonly http: HttpClient = axios.create();
+
+  protected readonly toolkit: XmlToolkit = new Xml2JsToolkit();
+
+  protected readonly caching: CacheAdapter = new MemoryCacheAdapter();
+
+  protected readonly certificates: CertificateRepository =
+    new NativeCertificateRepository(this.http, this.caching);
+
+  protected readonly soap: RemoteTransmissionRepository<NfeRemoteClient> =
+    new SoapRemoteTransmissionRepository(this.toolkit, this.certificates);
+
+  protected readonly transmitter = new NfeRemoteTransmitter(this.soap);
+
+  protected readonly signer = new EntitySigner(this.toolkit, this.certificates);
+
+  public constructor(protected readonly certificate: ReadCertificateRequest) {}
+
+  protected errors(): string[] | undefined {
+    return Reflect.getMetadata(ValidateErrorsMetadata, this) as
+      | string[]
+      | undefined;
+  }
+}
