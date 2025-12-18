a87bbbc74830d5f2e0d6fdfa783318b5fa95a372
Gustavo Lidani <gustavo@lidani.dev>
2025-12-18 15:15:03 -0300

    Refactor NFe XML and Transmission Pipelines to Use Updated Schemas Function
    - Updated the NfeXmlBuilderPipeline and NfeRemoteTransmitter classes to utilize the new schemas function for resolving schema directories, enhancing flexibility in schema management.
    - Modified related tests to ensure compatibility with the new schema resolution logic.

--- packages/nfe/src/domain/entities/transmission/schemas.ts
+++ packages/nfe/src/domain/entities/transmission/schemas.ts
@@ -1,10 +1,34 @@
-const schemas = {
+import { getCurrentFile } from '@nfets/core/shared/resolve-requires';
+import { existsSync } from 'node:fs';
+import { resolve } from 'node:path';
+
+const Schemas = {
   PL_009_V4: 'PL_009_V4',
   PL_010_V1: 'PL_010_V1',
   PL_010_V1_21: 'PL_010_V1.21',
 } as const;
 
-export const directory = process.env.NFETS_NFE_SCHEMAS_DIR ?? '';
+export const schemas = () => {
+  if (process.env.NFETS_NFE_SCHEMAS_DIR) {
+    return process.env.NFETS_NFE_SCHEMAS_DIR;
+  }
 
-export type Schema = (typeof schemas)[keyof typeof schemas];
-export default schemas;
+  const __filename = getCurrentFile();
+
+  const attempts = [
+    resolve(__filename, 'node_modules', 'nfets', 'packages', 'nfe', 'schemas'),
+    resolve(__filename, '../../../../', 'nfe', 'schemas'),
+    resolve(__filename, '../../../', 'nfe', 'schemas'),
+    resolve(__filename, '../../', 'nfe', 'schemas'),
+    resolve(__filename, '../', 'nfe', 'schemas'),
+  ];
+
+  for (const attempt of attempts) {
+    if (existsSync(attempt)) return attempt;
+  }
+
+  return __filename;
+};
+
+export type Schema = (typeof Schemas)[keyof typeof Schemas];
+export default Schemas;
