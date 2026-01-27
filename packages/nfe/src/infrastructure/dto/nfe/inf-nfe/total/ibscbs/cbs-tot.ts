b89dec94657ebf8614c4fa3bb8e8b92918e0bddb
fghinkel <felipehinkel@outlook.com>
2026-01-27 14:52:18 -0300

    `fix`: types

--- packages/nfe/src/infrastructure/dto/nfe/inf-nfe/total/ibscbs/cbs-tot.ts
+++ packages/nfe/src/infrastructure/dto/nfe/inf-nfe/total/ibscbs/cbs-tot.ts
@@ -1,20 +1,26 @@
 import { type CBSTot as ICBSTot } from '@nfets/nfe/domain/entities/nfe/inf-nfe/total';
 import { TransformDecimal } from '@nfets/core/application';
 import type { DecimalValue } from '@nfets/core/domain';
+import { IsOptional } from 'class-validator';
 
 export class CBSTot implements ICBSTot {
+  @IsOptional()
   @TransformDecimal({ fixed: 2 })
   public vDif: DecimalValue = '0.00';
 
+  @IsOptional()
   @TransformDecimal({ fixed: 2 })
   public vDevTrib: DecimalValue = '0.00';
 
+  @IsOptional()
   @TransformDecimal({ fixed: 2 })
   public vCBS: DecimalValue = '0.00';
 
+  @IsOptional()
   @TransformDecimal({ fixed: 2 })
   public vCredPres: DecimalValue = '0.00';
 
+  @IsOptional()
   @TransformDecimal({ fixed: 2 })
   public vCredPresCondSus: DecimalValue = '0.00';
 }
